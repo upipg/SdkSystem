ad8d61edfa7eb5c9d5a9696d7057b9cef0b8e1e6
varshantatray <varshant.atray@bumppy.com>
2023-11-21 21:47:52 +0530

    Initial commit

---
+++ app/src/main/java/com/app/sdkupipg/CompleteTransaction.java
@@ -0,0 +1,89 @@
+package com.app.sdkupipg;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class CompleteTransaction extends AppCompatActivity {
+    int flag;
+    GifImageView correct,wrong;
+    TextView txn_id,amount,merchant_name,brand_name,mobile,response,date,desc;
+    String Transaction_id,strAmount,strmerchnat_name,strbrandname,strdate,strmobile;
+    Button done;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.complete_transcation);
+
+        correct= findViewById(R.id.correct);
+        wrong= findViewById(R.id.wrong);
+        date= findViewById(R.id.date);
+
+        amount= findViewById(R.id.amount);
+        txn_id= findViewById(R.id.txn_id);
+        response= findViewById(R.id.response);
+        merchant_name= findViewById(R.id.merchant_name);
+        brand_name= findViewById(R.id.brand_name);
+        mobile= findViewById(R.id.mobile);
+        done= findViewById(R.id.done);
+        desc= findViewById(R.id.desc);
+
+
+        // Get the current date and time
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        strdate = sdf.format(new Date());
+
+
+        Intent i = getIntent();
+        flag= i.getExtras().getInt("flag");
+        if (i.getExtras().getString("txnid").isEmpty()||i.getExtras().getString("txnid").equals(null)) {
+            Transaction_id = i.getExtras().getString("ref_t_id");
+        }else {
+            Transaction_id = i.getExtras().getString("txnid");
+        }
+        strAmount= i.getExtras().getString("trAm");
+        strmerchnat_name= i.getExtras().getString("merchant_name");
+        strbrandname= i.getExtras().getString("BRname");
+        strmobile= i.getExtras().getString("mobile");
+
+        if (flag==1){
+    response.setText("Payment Success");
+    correct.setVisibility(View.VISIBLE);
+    wrong.setVisibility(View.GONE);
+}else{
+    response.setText("Payment Failed");
+    correct.setVisibility(View.GONE);
+    wrong.setVisibility(View.VISIBLE);
+}
+
+        mobile.setText(strmobile);
+        brand_name.setText(strbrandname);
+        merchant_name.setText(strmerchnat_name);
+        amount.setText("₹ "+strAmount);
+
+        date.setText(strdate);
+        desc.setText("Thanks for Choosing "+strbrandname);
+
+        txn_id.setText(Transaction_id);
+
+        done.setOnClickListener(v -> {
+            finish();
+        });
+
+    }
+
+
+}
---
+++ app/src/main/java/com/app/sdkupipg/Models/PG_Response_RES.java
@@ -0,0 +1,119 @@
+package com.app.sdkupipg.Models;
+
+import java.io.Serializable;
+
+public class PG_Response_RES  implements Serializable {
+  private String msg;
+
+  private Data data;
+
+  private Response response;
+
+  private String status;
+
+  public String getMsg() {
+    return this.msg;
+  }
+
+  public void setMsg(String msg) {
+    this.msg = msg;
+  }
+
+  public Data getData() {
+    return this.data;
+  }
+
+  public void setData(Data data) {
+    this.data = data;
+  }
+
+  public Response getResponse() {
+    return this.response;
+  }
+
+  public void setResponse(Response response) {
+    this.response = response;
+  }
+
+  public String getStatus() {
+    return this.status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public static class Data implements Serializable {
+    private String Message;
+
+    private String Id;
+
+    public String getMessage() {
+      return this.Message;
+    }
+
+    public void setMessage(String Message) {
+      this.Message = Message;
+    }
+
+    public String getId() {
+      return this.Id;
+    }
+
+    public void setId(String Id) {
+      this.Id = Id;
+    }
+  }
+
+  public static class Response implements Serializable {
+    private String ApprovalRefNo;
+
+    private String Status;
+
+    private String txnRef;
+
+    private String txnId;
+
+    private String responseCode;
+
+    public String getApprovalRefNo() {
+      return this.ApprovalRefNo;
+    }
+
+    public void setApprovalRefNo(String ApprovalRefNo) {
+      this.ApprovalRefNo = ApprovalRefNo;
+    }
+
+    public String getStatus() {
+      return this.Status;
+    }
+
+    public void setStatus(String Status) {
+      this.Status = Status;
+    }
+
+    public String getTxnRef() {
+      return this.txnRef;
+    }
+
+    public void setTxnRef(String txnRef) {
+      this.txnRef = txnRef;
+    }
+
+    public String getTxnId() {
+      return this.txnId;
+    }
+
+    public void setTxnId(String txnId) {
+      this.txnId = txnId;
+    }
+
+    public String getResponseCode() {
+      return this.responseCode;
+    }
+
+    public void setResponseCode(String responseCode) {
+      this.responseCode = responseCode;
+    }
+  }
+}
