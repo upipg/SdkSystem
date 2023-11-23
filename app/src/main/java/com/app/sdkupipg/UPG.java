557acffa4b31aeafed4fa4f478001c5ff82b2641
varshantatray <varshant.atray@bumppy.com>
2023-11-23 11:04:24 +0530

    latest

--- app/src/main/java/com/app/sdkupipg/UPG.java
+++ app/src/main/java/com/app/sdkupipg/UPG.java
@@ -427,7 +427,7 @@ public class UPG extends AppCompatActivity {
                     if (response.body().getStatus().equals("00")) {
 
 
-                            if (response.body().getResponse().getStatus().equals("SUCCESS")||response.body().getResponse().getStatus().equals("Success")){
+                            if (response.body().getResponse().getStatus().equals("Success")||response.body().getResponse().getStatus().equals("SUCCESS")){
 
                                 Intent i = new Intent(UPG.this, CT.class);
                                 i.putExtra("txnid", response.body().getResponse().getTxnId());
